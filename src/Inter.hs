c981742587b400354e78436eb4112265d8270fb4
hal arnold <halarnold2000@yahoo.com>
2015-11-05 18:14:38 -0800

    up to prob 13

--- src/Inter.hs
+++ src/Inter.hs
@@ -1,112 +1,139 @@
 module Inter where
 
-class Fluffy f where
-  furry :: (a -> b) -> f a -> f b
+class Functor' f where
+  fmap' :: (a -> b) -> f a -> f b
+
+-- | tests how to do fmap
+-- >>> fmap' (3*) [1,2,3]
+-- [3,6,9]
 
 -- Relative Difficulty: 1
-instance Fluffy [] where
-  furry = error "todo"
+instance Functor' [] where
+  fmap' f [] = []
+  fmap' f (x:xs) = f x : fmap' f xs
+
+-- | test: maybe for fmap'
+-- >>> fmap' (3+) (Just 3)
+-- Just 6
 
 -- Exercise 2
 -- Relative Difficulty: 1
-instance Fluffy Maybe where
-  furry = error "todo"
+instance Functor' Maybe where
+  fmap' f Nothing = Nothing
+  fmap' f (Just a) = Just (f a)
 
 -- Exercise 3
 -- Relative Difficulty: 5
-instance Fluffy ((->) t) where
-  furry = error "todo"
+
+-- | test: (->) r for fmap'
+-- >>> fmap' (3+) (*4) 6
+-- 27
+instance Functor' ((->) t) where
+  fmap' = (.)
+
+-- this shows that Either could be either left or right convention
 
 newtype EitherLeft b a = EitherLeft (Either a b)
 newtype EitherRight a b = EitherRight (Either a b)
 
 -- Exercise 4
 -- Relative Difficulty: 5
-instance Fluffy (EitherLeft t) where
-  furry = error "todo"
+instance Functor' (EitherLeft t) where
+  fmap' f (EitherLeft (Left x)) = EitherLeft (Left (f x))
+  fmap' f (EitherLeft (Right x))= EitherLeft (Right x)
 
 -- Exercise 5
 -- Relative Difficulty: 5
-instance Fluffy (EitherRight t) where
-  furry = error "todo"
+instance Functor' (EitherRight t) where
+  fmap' f (EitherRight (Right a)) = EitherRight (Right (f a))
+  fmap' f (EitherRight (Left x)) = EitherRight (Left x)
+  -- fmap' f (EitherRight z) = EitherRight z
+  --fmap' f x = x
 
-class Misty m where
-  banana :: (a -> m b) -> m a -> m b
-  unicorn :: a -> m a
+
+class Monad' m where
+  bind' :: (a -> m b) -> m a -> m b
+  return' :: a -> m a
   -- Exercise 6
   -- Relative Difficulty: 3
-  -- (use banana and/or unicorn)
-  furry' :: (a -> b) -> m a -> m b
-  furry' = error "todo"
+  -- (use bind' and/or return')
+  fmap'' :: (a -> b) -> m a -> m b
+  -- x = ma
+  fmap'' f  = bind' $ return' . f
 
+-- |
+--
 -- Exercise 7
 -- Relative Difficulty: 2
-instance Misty [] where
-  banana = error "todo"
-  unicorn = error "todo"
+instance Monad' [] where
+  bind' f x = concat (fmap' f x)
+  return' x = [x]
 
 -- Exercise 8
 -- Relative Difficulty: 2
-instance Misty Maybe where
-  banana = error "todo"
-  unicorn = error "todo"
+instance Monad' Maybe where
+  bind' f Nothing = Nothing
+  bind' f (Just x)= f x
+  return' = Just
 
 -- Exercise 9
 -- Relative Difficulty: 6
-instance Misty ((->) t) where
-  banana = error "todo"
-  unicorn = error "todo"
+instance Monad' ((->) t) where
+  bind' f x = \y -> f (x y) y
+  return' = const
 
 -- Exercise 10
 -- Relative Difficulty: 6
-instance Misty (EitherLeft t) where
-  banana = error "todo"
-  unicorn = error "todo"
+instance Monad' (EitherLeft t) where
+  bind' f (EitherLeft (Left x)) = f x
+  bind' f (EitherLeft (Right x))= EitherLeft (Right x)
+  return' x = EitherLeft (Left x)
 
 -- Exercise 11
 -- Relative Difficulty: 6
-instance Misty (EitherRight t) where
-  banana = error "todo"
-  unicorn = error "todo"
+instance Monad' (EitherRight t) where
+  bind' f (EitherRight (Right x)) = f x
+  bind' f (EitherRight (Left y))  = EitherRight (Left y)
+  return' x = EitherRight (Right x)
 
 -- Exercise 12
 -- Relative Difficulty: 3
-jellybean :: (Misty m) => m (m a) -> m a
-jellybean = error "todo"
+join' :: (Monad' m) => m (m a) -> m a
+join' mma = bind' id mma
 
 -- Exercise 13
 -- Relative Difficulty: 6
-apple :: (Misty m) => m a -> m (a -> b) -> m b
-apple = error "todo"
+apple :: (Monad' m) => m a -> m (a -> b) -> m b
+apple = undefined
 
 -- Exercise 14
 -- Relative Difficulty: 6
-moppy :: (Misty m) => [a] -> (a -> m b) -> m [b]
+moppy :: (Monad' m) => [a] -> (a -> m b) -> m [b]
 moppy = error "todo"
 
 -- Exercise 15
 -- Relative Difficulty: 6
 -- (bonus: use moppy)
-sausage :: (Misty m) => [m a] -> m [a]
+sausage :: (Monad' m) => [m a] -> m [a]
 sausage = error "todo"
 
 -- Exercise 16
 -- Relative Difficulty: 6
--- (bonus: use apple + furry')
-banana2 :: (Misty m) => (a -> b -> c) -> m a -> m b -> m c
-banana2 = error "todo"
+-- (bonus: use apple + fmap'')
+bind'2 :: (Monad' m) => (a -> b -> c) -> m a -> m b -> m c
+bind'2 = error "todo"
 
 -- Exercise 17
 -- Relative Difficulty: 6
--- (bonus: use apple + banana2)
-banana3 :: (Misty m) => (a -> b -> c -> d) -> m a -> m b -> m c -> m d
-banana3 = error "todo"
+-- (bonus: use apple + bind'2)
+bind'3 :: (Monad' m) => (a -> b -> c -> d) -> m a -> m b -> m c -> m d
+bind'3 = error "todo"
 
 -- Exercise 18
 -- Relative Difficulty: 6
--- (bonus: use apple + banana3)
-banana4 :: (Misty m) => (a -> b -> c -> d -> e) -> m a -> m b -> m c -> m d -> m e
-banana4 = error "todo"
+-- (bonus: use apple + bind'3)
+bind'4 :: (Monad' m) => (a -> b -> c -> d -> e) -> m a -> m b -> m c -> m d -> m e
+bind'4 = error "todo"
 
 newtype State s a = State {
   state :: (s -> (s, a))
@@ -114,11 +141,11 @@ newtype State s a = State {
 
 -- Exercise 19
 -- Relative Difficulty: 9
-instance Fluffy (State s) where
-  furry = error "todo"
+instance Functor' (State s) where
+  fmap' = error "todo"
 
 -- Exercise 20
 -- Relative Difficulty: 10
-instance Misty (State s) where
-  banana = error "todo"
-  unicorn = error "todo"
+instance Monad' (State s) where
+  bind' = error "todo"
+  return' = error "todo"
